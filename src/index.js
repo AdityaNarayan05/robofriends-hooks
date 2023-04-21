361ef2de6e535e158b1ac596eb3d45882f20e6b4
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-04-21 23:46:34 +0530

    Update Library and Project directories

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+// import registerServiceWorker from './registerServiceWorker';
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 
 
-
-
-
-
-
-
-
-
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
