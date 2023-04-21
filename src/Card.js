5de46b9071320454655df9dc205f8f1ce54a9e8e
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-04-21 18:14:09 +0530

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Card=()=>{
+    return(
+        <div>
+            <img alt='robots' src ='https://robohash.org/mala?200*200' />
+            <div>
+                <h2> Aditya Narayan Jaiswal </h2>
+                <p>nlaxmi886@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
